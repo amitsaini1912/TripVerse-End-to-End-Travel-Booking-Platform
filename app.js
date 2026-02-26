1fa3d498800bb4a3845810593155a5782d20a72d
amitsaini1912 <amitkumarsainig9@gmail.com>
2026-02-26 14:36:30 +0530

    WanterLust Project files Loaded to Upgrade this to full end to end booking system

--- app.js
+++ app.js
@@ -66,11 +66,12 @@ store.on("error", () => {
 
 // Session & Cookies
 const sessionOptions = {
+  store: store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    expires: Date.now() * 7 * 24 * 60 * 60 * 1000,
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
   },
